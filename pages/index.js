e19576df4cd588e7daecaf179b3063cce94b8e1a
Kim <10829539@uvu.edu>
2023-05-04 22:26:27 -0600

    added gradient color background animation to the main homepage index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import Navbar from '../components/Navbar';
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,20 +11,48 @@ const greetingVariants = {
 };
 
 export default function Home() {
+  const controls = useAnimation();
+
+  const animateBackground = async () => {
+    while (true) {
+      await controls.start({
+        backgroundPositionX: '100%',
+        transition: { duration: 5, ease: 'linear' },
+      });
+      await controls.start({
+        backgroundPositionX: '0%',
+        transition: { duration: 5, ease: 'linear' },
+      });
+    }
+  };
+
+  useEffect(() => {
+    controls.start(animateBackground);
+  }, [controls]);
+
+  const gradientStyle = {
+    background: `linear-gradient(314deg, #ffd6ff, #e7c6ff, #c8b6ff, #b8c0ff, #bbd0ff)`,
+    backgroundSize: '1000% 1000%',
+  };
+
   return (
-    <div className="bg-quinary min-h-screen">
+    <motion.div
+      className="min-h-screen"
+      style={gradientStyle}
+      animate={controls}
+    >
       <title>Home</title>
       <Navbar />
       <main className="flex justify-center items-center h-full">
         <motion.h1
-          className="text-5xl font-bold"
+          className="text-5xl font-bold text-black"
           variants={greetingVariants}
           initial="hidden"
           animate="visible"
         >
-          Welcome to My Portfolio
+            Welcome to My Portfolio
         </motion.h1>
       </main>
-    </div>
+    </motion.div>
   );
 }
